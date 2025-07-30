67c596996d2b586f0d7bf5c75d504c58c365b34e
Kevin Gabriel Florentino <kevingabriel786@gmail.com>
2025-07-30 16:56:22 +0700

    Update MyBCA Adjustment

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,29 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { ChakraProvider, defaultSystem } from '@chakra-ui/react'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App.tsx'
+import { HashRouter, Route, Routes } from 'react-router-dom'
 import './index.css'
 import { UserProvider } from './UserContext.tsx'
+import BC from './pages/BC/index.tsx'
+import CM from './pages/CM/index.tsx'
+import HomeBCM from './pages/homeBCM.tsx'
+import SelfSurveyAreaKerjaK3 from './pages/K3/SelfSurveyAreaKerjaK3.tsx'
+import SelfSurveyPeralatanK3 from './pages/K3/SelfSurveyPeralatanK3.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <React.StrictMode>
     <ChakraProvider value={defaultSystem}>
       <UserProvider>
-        <BrowserRouter basename="/">
-          <App />
-        </BrowserRouter>
+        <HashRouter>
+        <Routes>
+          <Route path="/" element={<HomeBCM/>}/>
+          <Route path="BC" element={<BC/>}/>
+          <Route path="CM" element={<CM/>}/>
+          <Route path="K3/SelfSurveyAreaKerja" element={<SelfSurveyAreaKerjaK3/>}/>
+          <Route path="K3/SelfSurveyPeralatan" element={<SelfSurveyPeralatanK3/>}/>
+        </Routes>
+        </HashRouter>
       </UserProvider>
     </ChakraProvider>
-  </StrictMode>
+  </React.StrictMode>
 )
